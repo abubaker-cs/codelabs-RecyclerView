3e8cca97950522f26ef76933573846dd06ae3c97
abubaker <mohammad_abubaker@yahoo.com>
2021-02-16 22:53:58 +0500

    adding comments

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepNightAdapter.kt
@@ -8,42 +8,58 @@ import com.example.android.trackmysleepquality.R
 import com.example.android.trackmysleepquality.TextItemViewHolder
 import com.example.android.trackmysleepquality.database.SleepNight
 
-//
+/**
+ * Important: The "core task" in implementing a RecyclerView is creating the Adapter.
+ *
+ * The Adapter creates a ViewHolder and FILLS it with data for the RecyclerView to Display.
+ */
+
+// We will extend our class with : RecyclerView.Adapter<***>()
+// *** Define which view holder to use
 class SleepNightAdapter : RecyclerView.Adapter<TextItemViewHolder>() {
 
-    //
+    // We are creating a LIST to hold the DATA
     var data = listOf<SleepNight>()
         set(value) {
             field = value
             notifyDataSetChanged()
         }
 
-    //
+    // Total number of ROWS for the RecyclerView to display
     override fun getItemCount() = data.size
 
 
-    //
+    // onBindViewHolder() is used to create a single instance of a ROW
+    // It requires the VIEW in which data will be PLACED, and the POSITION of ROW from INCOMING data
     override fun onBindViewHolder(holder: TextItemViewHolder, position: Int) {
 
-        //
+        // Actual POSITION of the ROW in the INCOMING data
         val item = data[position]
 
-        //
+        // Since our ViewHolder, i.e. text_item_view.xml file contains a <TextView>, thus
+        // we will use it to display our data: sleepQuality
         holder.textView.text = item.sleepQuality.toString()
 
     }
 
-    //
+    // onCreateViewHolder() is called when a RecyclerView needs a ViewHolder
+    // It returns a ViewHolder based on two parameters:
+    // 1. parent: RecyclerView itself
+    // 2. viewType: It is used when there are multiple views in the SAME recyclerView, i.e. an image, text views and video
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TextItemViewHolder {
 
-        //
+        // 1. layoutInflater knows HOW to create views from XML files.
+        // 2. In our situation, context = RecyclerView itself.
         val layoutInflater = LayoutInflater.from(parent.context)
 
-        //
+        // 1. Reference to our text_item_view.xml FILE.
+        // 2. Parent = ViewGroup = RecyclerView itself.
+        // 3. RecyclerView automatically adds our item to the hierarchy, so there is no need for us to use attachToRoot
         val view = layoutInflater
                 .inflate(R.layout.text_item_view, parent, false) as TextView
 
-        //
+        // It will return view based on our custom class: TextItemViewHolder
+        // Please refer to our temporary ViewHolder class in Util.kt file
         return TextItemViewHolder(view)
 
     }
